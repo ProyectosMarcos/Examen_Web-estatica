c060ac8072c5301b96bb2948fcf8727835371338
ProyectosMarcos <m-fernandez988@hotmail.com>
2024-05-16 03:35:26 -0300

    navbar-update

---
+++ scripts/components/form.js
@@ -0,0 +1,5 @@
+export function formValidation() {
+  const form = document.querySelector('.form');
+  const inputs = document.querySelector('.form [required]');
+  console.log(inputs);
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
+import { formValidation } from './components/form.js';
 import { initModal } from './components/modal.js';
 import { initModalSearch } from './components/modalSearch.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   initModal();
   initModalSearch();
+  formValidation();
 });
